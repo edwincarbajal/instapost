11251cdfd5b759fd4c7da5b7c541244565798ce0
edwincarbajal <exaviercarbajal@gmail.com>
2017-05-25 19:09:58 -0400

    Add S3 host name to production and development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # AWS S3
   config.paperclip_defaults = {
   storage: :s3,
+  s3_host_name: ENV.fetch('S3_HOST_NAME'),
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
