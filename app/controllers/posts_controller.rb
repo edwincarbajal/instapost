fbf38449112aa61cc2a103eca49015b7da33ef76
edwincarbajal <exaviercarbajal@gmail.com>
2017-05-26 17:44:48 -0400

    Complete CRUD for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :get_posts, only: [:index]
 
   def index
@@ -12,6 +12,9 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -19,6 +22,19 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
