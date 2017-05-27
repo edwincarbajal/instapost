d5cb588d2fb486269151abb64f647994509f4d67
edwincarbajal <exaviercarbajal@gmail.com>
2017-05-27 18:59:58 -0400

    Restrict users to autherized actions only

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
   before_action :get_posts, only: [:index]
 
   def index
@@ -10,14 +11,14 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def edit
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "You post has been created!"
       redirect_to root_path
@@ -55,5 +56,12 @@ class PostsController < ApplicationController
 
     def post_params
       params.require(:post).permit(:caption, :image)
+    end
+
+    def owned_post
+      unless current_user === @post.user
+        flash[:alert] = "That post doesn't belong to you!"
+        redirect_to root_path
+      end
     end
 end
