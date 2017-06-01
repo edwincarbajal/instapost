670393993bdaa02bb91db2640852dc3d996d1b00
edwincarbajal <exaviercarbajal@gmail.com>
2017-06-01 01:57:40 -0400

    Create profile controller and views. Add avatar and bio field to user model migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :user_name, presence: true, length: { minimum: 4,
                                                   maximum: 16 }
+  has_attached_file :avatar, styles: { medium:'152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users, controllers: { registrations: 'registrations' }
+  get ':user_name', to: 'profiles#show', as: :profile
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
   resources :posts do
     resources :comments
   end
