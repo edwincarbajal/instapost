c3b865593ee4d0d9c63548f7815bd493daa69f3a
edwincarbajal <exaviercarbajal@gmail.com>
2017-06-12 22:44:13 -0400

    Add index page for notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,18 @@
 class NotificationsController < ApplicationController
+  before_action :get_notifications, only: [:index]
+
+  def index
+  end
+
   def link_through
     @notification = Notification.find(params[:id])
     @notification.update(read: true)
     redirect_to post_path(@notification.post)
   end
+
+  private
+
+    def get_notifications
+      @notifications = current_user.notifications
+    end
 end
