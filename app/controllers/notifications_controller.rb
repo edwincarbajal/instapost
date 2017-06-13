688f4e4fe845be2d4cfaced55572a12163dc63fe
edwincarbajal <exaviercarbajal@gmail.com>
2017-06-12 21:13:42 -0400

    Add read/unread comment notifications

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,7 @@
+class NotificationsController < ApplicationController
+  def link_through
+    @notification = Notification.find(params[:id])
+    @notification.update(read: true)
+    redirect_to post_path(@notification.post)
+  end
+end
