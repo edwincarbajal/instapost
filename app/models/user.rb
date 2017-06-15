ad2ae8fcf747f13a1ef7de7fd7b8e62e6651b2ed
edwincarbajal <exaviercarbajal@gmail.com>
2017-06-15 15:15:37 -0400

    Create Follow model and associations with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,18 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
+
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
 end
