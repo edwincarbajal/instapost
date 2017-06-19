093ec7bae680314d7b1aba19711c016a60ff28f3
edwincarbajal <exaviercarbajal@gmail.com>
2017-06-18 22:02:43 -0400

    Create notifications for likes/follow

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -10,6 +10,13 @@ class NotificationsController < ApplicationController
     redirect_to post_path(@notification.post)
   end
 
+  def follow_link_through
+    @notification = Notification.find(params[:id])
+    notified_by = User.find(@notification.notified_by_id)
+    @notification.update(read: true)
+    redirect_to profile_path(notified_by.user_name)
+  end
+
   private
 
     def get_notifications
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   get 'notifications', to: 'notifications#index'
   get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
+  get 'notifications/:id/follow_link_through', to: 'notifications#follow_link_through', as: :follow_link_through
   post ':user_name/follow_user', to: 'relationships#follow_user', as: :follow_user
   post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
   get 'explore', to: 'posts#browse', as: :browse_posts
