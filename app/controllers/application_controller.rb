f3ff05639d33c99f64335730f98a35a15af62e68
edwincarbajal <exaviercarbajal@gmail.com>
2019-10-05 18:19:58 -0400

    Remove guest feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  helper_method :current_user
+
+   def current_user
+     super
+   end
+
+   private
+     #
+     # def guest_user
+     #   User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
+     # end
+     #
+     # def create_guest_user
+     #   user = User.new { |user| user.guest = true }
+     #   user.email = "guest_#{Time.now.to_i}#{rand(99)}@example.com"
+     #   user.user_name = "guest_user"
+     #   user.save(:validate => false)
+     #   user
+     # end
 end
